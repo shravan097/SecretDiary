3ef2cae93bfb04c091d1917c6c394cd7edf461a1
Shravan Dhakal <sadijdhakal@gmail.com>
2017-04-21 13:30:35 -0400

    Update settings.py

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o%b44dq%obv%vn#i0^$h*idyx4-@&54gfkrp2nszsv8meszusm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.1.5']
+ALLOWED_HOSTS = []
 
 
 # Application definition
